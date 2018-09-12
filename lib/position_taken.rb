3824e2da87d748c9b340cb1ba85a8bd5141c7ea5
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-12 20:53:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 def position_taken? (board, index)
   if (board[index] ==  "") || (board[index] == " ") || (board[index] == NIL)
     FALSE
+    elsif (board[index] ==  "X") || (board[index] == "O")
+    TRUE
   end
 end
   
